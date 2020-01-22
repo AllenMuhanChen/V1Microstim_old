e2714cc65f9f8f49bc51c4b71c1992a6166e3db3
Allen Chen <allenmuhanchen@gmail.com>
2020-01-22 13:36:21 -0500

    Current Broken Version -- OsUtil Error

---
+++ xper-train/xper-example/src/org/xper/example/classic/EStimSpecGenerator.java
@@ -0,0 +1,32 @@
+
+package org.xper.example.classic;
+import org.xper.rfplot.EStimSpec;
+
+public class EStimSpecGenerator {
+	public static EStimSpec generate () {
+		EStimSpec e = new EStimSpec();
+		e.set_id(0);
+		e.set_chan(1);
+		e.set_trig_src("");
+		e.set_num_pulses(1);
+		e.set_pulse_train_period(0);
+		e.set_post_stim_refractory_period(0);
+		e.set_stim_shape("Biphasic");
+		e.set_stim_polarity("Cathodic");
+		e.set_d1(100);
+		e.set_d2(100);
+		e.set_dp(0);
+		e.set_a1(100);
+		e.set_a2(100);
+		e.set_pre_stim_amp_settle(0);
+		e.set_post_stim_amp_settle(0);
+		e.set_maintain_amp_settle_during_pulse_train(0);
+		e.set_post_stim_charge_recovery_on(0);
+		e.set_post_stim_charge_recovery_off(0);
+		return e;
+	}
+
+	public EStimSpec generateStimSpec() {
+		return EStimSpecGenerator.generate();
+	}
+}
---
+++ xper-train/xper/app/org/xper/app/experiment/test/RandomGeneration.java
@@ -0,0 +1,111 @@
+package org.xper.app.experiment.test;
+
+import org.xper.Dependency; 
+import org.xper.example.classic.EStimSpecGenerator;
+import org.xper.exception.VariableNotFoundException;
+import org.xper.experiment.StimSpecGenerator;
+import org.xper.rfplot.BlockSpec;
+import org.xper.rfplot.EStimSpec;
+import org.xper.time.TimeUtil;
+import org.xper.util.DbUtil;
+import java.util.Arrays;
+
+public class RandomGeneration {
+	@Dependency
+	DbUtil dbUtil;
+	@Dependency
+	TimeUtil globalTimeUtil;
+	@Dependency
+	StimSpecGenerator generator;
+
+	@Dependency
+	int taskCount;
+
+	public int getTaskCount() {
+		return taskCount;
+	}
+
+	public void setTaskCount(int taskCount) {
+		this.taskCount = taskCount;
+	}
+	
+	public void generate() {
+		System.out.print("Generating ");
+		long genId = 1;
+		try {
+			genId = dbUtil.readReadyGenerationInfo().getGenId() + 1;
+		} catch (VariableNotFoundException e) {
+			dbUtil.writeReadyGenerationInfo(genId, 0);
+		}
+		//AC: Block Logic
+		long blockId = 1;
+		BlockSpec block = dbUtil.readBlockSpec(blockId);
+		//blockRef: c-catch trial, v-vstim only, e-estim only, b-both
+		char blockref[] = new char[taskCount];
+		Arrays.fill(blockref, 0, block.get_num_catches()-1, 'c');
+		Arrays.fill(blockref, block.get_num_catches(), block.get_num_catches()+block.get_num_stims_only()-1, 'v');
+		Arrays.fill(blockref, block.get_num_catches()+block.get_num_stims_only(), block.get_num_catches()+block.get_num_stims_only()+block.get_num_estims_only()-1, 'e');		
+		Arrays.fill(blockref, block.get_num_catches()+block.get_num_stims_only()+block.get_num_estims_only(), block.get_num_catches()+block.get_num_stims_only()+block.get_num_estims_only()+block.get_num_both()-1, 'b');	
+	
+		if (block.get_shuffle() == "yes") {
+		//Shuffle Code Here	
+		}
+		//
+		for (int i = 0; i < taskCount; i++) {
+			if (i % 10 == 0) {
+				System.out.print(".");
+			}
+			String spec = generator.generateStimSpec();
+			//AC
+			long stimId;
+			int estimId =1;//STILL NEED TO ADD LOGIC FOR STIM PARAMETERS
+			long taskId = globalTimeUtil.currentTimeMicros();
+			if (blockref[i]=='c'){
+				stimId=0;
+				estimId=0;
+			}else if(blockref[i]=='v') {
+				stimId=taskId;
+				estimId=0;
+			}else if(blockref[i]=='e') {
+				stimId=0;
+				estimId=1;
+			}else {
+				stimId=taskId;
+				estimId=1;
+			}
+																			//added estimId
+			dbUtil.writeStimSpec(taskId, spec);								
+			dbUtil.writeTaskToDo(taskId, stimId, estimId, -1, genId);		//Added estimId to db
+			EStimSpec e = EStimSpecGenerator.generate();					//Generate EStimSpec
+			dbUtil.writeEStimSpec(e);										//Write EStimspec class to db
+			//
+		}
+		dbUtil.updateReadyGenerationInfo(genId, taskCount);
+		System.out.println("done.");
+	}
+
+	public DbUtil getDbUtil() {
+		return dbUtil;
+	}
+
+	public void setDbUtil(DbUtil dbUtil) {
+		this.dbUtil = dbUtil;
+	}
+
+	
+	public TimeUtil getGlobalTimeUtil() {
+		return globalTimeUtil;
+	}
+
+	public void setGlobalTimeUtil(TimeUtil globalTimeUtil) {
+		this.globalTimeUtil = globalTimeUtil;
+	}
+
+	public StimSpecGenerator getGenerator() {
+		return generator;
+	}
+
+	public void setGenerator(StimSpecGenerator generator) {
+		this.generator = generator;
+	}
+}
