7dc0b886a4032f8160bc97b803ac2f06519dfdab
Allen Chen <allenmuhanchen@gmail.com>
2020-01-23 10:44:19 -0500

    Update TaskDoneEntry.java
    Replacing with version from sach

--- xper-train/xper/src/org/xper/db/vo/TaskDoneEntry.java
+++ xper-train/xper/src/org/xper/db/vo/TaskDoneEntry.java
@@ -4,6 +4,24 @@ public class TaskDoneEntry {
 	long tstamp;
 	long taskId;
 	int part_done;
+	
+	public TaskDoneEntry(){
+		super();
+	}
+	
+	public TaskDoneEntry(String[] dbInputs){
+		// AWC August2017. This constructor is meant to be called after querying the database and returning a String[]. The order of data in the String[] will exactly match the column names in ExpLog table, 
+		//    so ANY CHANGES MADE TO ExpLog STRUCTURE MUST BE REFLECTED IN THE INPUTS OF THIS FUNCTION
+		super();
+		
+		String tstampS = 	dbInputs[0];
+		String taskIdS =	dbInputs[1];
+		String part_doneS =	dbInputs[2];
+		setTstamp(Long.parseLong(tstampS));
+		setTaskId(Long.parseLong(taskIdS));
+		setPart_done(Integer.parseInt(part_doneS));
+	}
+	
 	public int getPart_done() {
 		return part_done;
 	}
